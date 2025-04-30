ed3aacd53253c6ac5dde8c49ed5fe55b8dbc32c5
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2025-04-30 16:18:41 +0530

    Refactor menu toggle functionality and improve CSS formatting

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
-//    Toggle mobile menu visibility
-    document.body.classList.toggle("show-mobile-menu");
+  //    Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
 });
 
 // Close menu when the close button is clicked
